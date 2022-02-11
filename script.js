a2b04ead8e15805b141bf3fd7780aa0cbf0abb1c
YasuyukiKuno <kunoyasuyuki@yahoo.co.jp>
2022-02-11 14:26:07 +0900

    ボタンの調整

--- script.js
+++ script.js
@@ -1,29 +1,147 @@
-const target = document.getElementById('mitarashi') // アニメーションの対象
-const startX = 50 // X座標の始点
-const startY = 50 // Y座標の始点
-const endX = 500 // X座標の目標点
-const endY = 150 // Y座標の目標点
-const diffX = endX - startX // X座標の変化量
-const diffY = endY - startY // Y座標の変化量
-const time = 1000 // 変化にかける時間 (ミリ秒)
+// 開始時のモーダル
+const startDialog = document.getElementById('startModal')
+const startButton = document.getElementById('start')
+// 結果時のモーダル
+const resultDialog = document.getElementById('resultModal')
+const resultTitle = document.getElementById('resultModalLabel')
+const resultBody = document.getElementById('resultBody')
+const modalWrapper = document.getElementById('modal-wrapper')
 
-// アップデートを実行する
-requestAnimationFrame(update)
+const firstBlock = document.getElementById('blockAbove')
+const secondBlock = document.getElementById('blockSecond')
+const thirdBlock = document.getElementById('blockThird')
+const fourthBlock = document.getElementById('blockFourth')
 
-function update(timestamp) {
-  // 進捗率を算出
-  progress = timestamp / time
+// おじさんを1秒後に1秒かけて座標(500, 150)へアニメーションさせる
+//
+/**
+ * 対象オブジェクトをアニメーションして移動。
+ * @param {string} targetID スクロール時間のミリ秒。
+ * @param {number} time アニメーションさせる時間（ミリ秒）。
+ * @param {number} endX X座標の目標値。
+ * @param {number} endY Y座標の目標値。
+ * @param {number} delay アニメーションを開始するまでの遅延時間です（ミリ秒）。
+ */
+function animateTranslate(targetID, time, endX, endY, delay) {
+  const target = document.getElementById(targetID) // アニメーションの対象
+  const style = getComputedStyle(target) // 対象のスタイルを取得
+  const reg = /matrix\((.*)\)/ // 不要な文字列を取り除くための正規表現
+  const transform = style.transform.match(reg)[1].split(',') // transform を配列として取得
+  const startX = parseFloat(transform[4]) // X座標の始点
+  const startY = parseFloat(transform[5]) // Y座標の始点
+  const diffX = endX - startX // X座標の変化量
+  const diffY = endY - startY // Y座標の変化量
+  let progress = 0
 
-  // 進捗率が100%を超えないよう丸める
-  progress = Math.min(progress, 1)
+  // アップデートを実行する
+  requestAnimationFrame(update)
 
-  // 値を算出し反映する
-  const resultX = startX + diffX * progress // X座標
-  const resultY = startY + diffY * progress // Y座標
-  target.style.transform = `translate( ${resultX}px, ${resultY}px )`
+  function update(timestamp) {
+    // 遅延時間を含めた進捗率を算出
+    progress = (timestamp - delay) / time
 
-  // 進捗が1未満の場合はアップデートを再実行する
-  if (progress < 1) {
-    requestAnimationFrame(update)
+    // 進捗率が100%を超えないよう丸める
+    progress = Math.min(progress, 1)
+
+    // 進捗率がプラスの場合のみ値を算出し反映する
+    if (progress >= 0) {
+      const resultX = startX + diffX * progress // X座標
+      const resultY = startY + diffY * progress // Y座標
+      target.style.transform = `translate( ${resultX}px, ${resultY}px )`
+    }
+
+    // 進捗が1未満の場合はアップデートを再実行する
+    if (progress < 1) {
+      requestAnimationFrame(update)
+    }
   }
 }
+
+// ロード時にモーダル表示
+window.onload = () => {
+  startDialog.ariaModal = 'true'
+  startDialog.role = 'dialog'
+  startDialog.style = 'display: block;'
+  startDialog.classList.add('show')
+}
+
+// モーダルの表示の設定
+const modal = function () {
+  resultDialog.ariaModal = 'true'
+  resultDialog.role = 'dialog'
+  resultDialog.style = 'display: block;'
+  resultDialog.classList.add('show')
+  resultDialog.setAttribute('aria-hidden', 'true')
+}
+
+// スタート押下時
+startButton.addEventListener('click', function () {
+  startDialog.remove()
+  modalWrapper.style.visibility = 'hidden'
+  animateTranslate('oji', 10000, -800, 0, 1000)
+})
+;('use strict')
+const body = document.querySelector('body')
+const background = document.querySelector('.background')
+const countDisplay = document.querySelector('.countDisplay')
+const result = document.querySelector('.result')
+
+let count = 0
+
+// パソコンかスマートフォンか判定
+const eventType = window.ontouchstart !== null ? 'click' : 'touchstart'
+const addCount = function (e) {
+  // クリックをカウント・表示
+  count++
+  countDisplay.textContent = count
+  // クリック時のアニメーション
+  let x = e.pageX
+  let y = e.pageY
+  const mash = document.createElement('div')
+  mash.style.top = y + 'px'
+  mash.style.left = x + 'px'
+  document.body.appendChild(mash)
+  mash.className = 'mash'
+  mash.addEventListener('animationend', () => {
+    mash.parentNode.removeChild(mash)
+  })
+
+  if (count >= 5) {
+    document.getElementById('blockAbove').style.visibility = 'hidden'
+  }
+  if (count >= 15) {
+    document.getElementById('blockSecond').style.visibility = 'hidden'
+  }
+  if (count >= 25) {
+    document.getElementById('blockThird').style.visibility = 'hidden'
+  }
+  if (count >= 35) {
+    document.getElementById('blockFourth').style.visibility = 'hidden'
+  }
+}
+// 背景を縮めるアニメーション
+const shrinkAnim = function () {
+  countDisplay.classList.remove('blink')
+  body.removeEventListener(eventType, shrinkAnim)
+  body.addEventListener(eventType, addCount)
+  background
+    .animate(
+      {
+        width: ['350px', '0px'],
+        height: ['350px', '0px'],
+        opacity: [1, 0.5, 1],
+        offset: [0, 0.9],
+      },
+      { duration: 5000, fill: 'forwards' }
+    )
+    .finished // ゲーム終了後の処理
+    .then(() => {
+      body.removeEventListener(eventType, addCount)
+      result.textContent = 'Click to return'
+      result.classList.add('blink')
+      result.addEventListener('click', () => {
+        location.reload()
+      })
+    })
+}
+body.addEventListener(eventType, shrinkAnim)
