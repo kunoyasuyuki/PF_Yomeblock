8f9b4721c48a4dd2e261ce7894d718ba91ec9ba7
YasuyukiKuno <kunoyasuyuki@yahoo.co.jp>
2022-02-13 00:47:30 +0900

    fix

--- script.js
+++ script.js
@@ -15,51 +15,10 @@ const fourthBlock = document.getElementById('blockFourth')
 const fadeIn = document.getElementById('fade')
 
 var count = 0
-// keyframsを作成する。htmlでトリガー作る。開始ボタン押されたら、cssアニメ
 
-// おじさんを1秒後に1秒かけて座標(500, 150)へアニメーションさせる
-//
-/**
- * 対象オブジェクトをアニメーションして移動。
- * @param {string} targetID スクロール時間のミリ秒。
- * @param {number} time アニメーションさせる時間（ミリ秒）。
- * @param {number} endX X座標の目標値。
- * @param {number} endY Y座標の目標値。
- * @param {number} delay アニメーションを開始するまでの遅延時間です（ミリ秒）。
- */
-function animateTranslate(targetID, time, endX, endY, delay) {
-  const target = document.getElementById(targetID) // アニメーションの対象
-  const style = getComputedStyle(target) // 対象のスタイルを取得
-  const reg = /matrix\((.*)\)/ // 不要な文字列を取り除くための正規表現
-  const transform = style.transform.match(reg)[1].split(',') // transform を配列として取得
-  const startX = parseFloat(transform[4]) // X座標の始点
-  const startY = parseFloat(transform[5]) // Y座標の始点
-  const diffX = endX - startX // X座標の変化量
-  const diffY = endY - startY // Y座標の変化量
-  let progress = 0
-
-  // アップデートを実行する
-  requestAnimationFrame(update)
-
-  function update(timestamp) {
-    // 遅延時間を含めた進捗率を算出
-    progress = (timestamp - delay) / time
-
-    // 進捗率が100%を超えないよう丸める
-    progress = Math.min(progress, 1)
-
-    // 進捗率がプラスの場合のみ値を算出し反映する
-    if (progress >= 0) {
-      const resultX = startX + diffX * progress // X座標
-      const resultY = startY + diffY * progress // Y座標
-      target.style.transform = `translate( ${resultX}px, ${resultY}px )`
-    }
-
-    // 進捗が1未満の場合はアップデートを再実行する
-    if (progress < 1) {
-      requestAnimationFrame(update)
-    }
-  }
+function move() {
+  const ojiImg = document.getElementById('ojiImg')
+  ojiImg.classList.add('ojiMove')
 }
 
 // ロード時にモーダル表示
@@ -82,8 +41,8 @@ const modal = function () {
 // スタート押下時
 startButton.addEventListener('click', function () {
   startDialog.remove()
+  move()
   modalWrapper.style.visibility = 'hidden'
-  animateTranslate('oji', 5000, -800, 0, 1000)
   setTimeout('gameResult()', 5000)
 })
 ;('use strict')
@@ -122,6 +81,8 @@ const addCount = function (e) {
   }
   if (count >= 35) {
     document.getElementById('blockFourth').style.visibility = 'hidden'
+    document.getElementById('blockFifth').style.visibility = 'hidden'
+    document.getElementById('blockSixth').style.visibility = 'hidden'
   }
 }
 
@@ -138,12 +99,13 @@ function gameResult() {
     fadeIn.innerText = 'やばい！'
     resultTitle.innerText = '嫁めっちゃ怒ってるぞww！！'
     resultBody.innerHTML =
-      'あっ、嫁からLINEだ。「GPSついてるからね？。。」あっっ！？（汗）。。。。またチャレンジしてくれ！'
+      'あっ、嫁からLINEだ。「GPSついてるからね？。。」あっバレた！（汗）。。。。またチャレンジしてくれ！'
   }
   if (num > 25) {
     fadeIn.innerText = '激突！！'
     resultTitle.innerText = '嫁ブロック突破失敗だ！！'
-    resultBody.innerHTML = 'あっ、嫁から留守電だ。。。20件も来てるwww 今日は撤退だ！！またチャレンジしてくれ！'
+    resultBody.innerHTML =
+      'あっ、嫁から留守電だ。。。まじかよ。20件も来てるよwww 今日は撤退だ！！またチャレンジしてくれ！'
   }
   if (num > 34) {
     fadeIn.innerText = '突破！！'
@@ -172,7 +134,6 @@ const shrinkAnim = function () {
     .finished // ゲーム終了後の処理
     .then(() => {
       body.removeEventListener(eventType, addCount)
-      result.textContent = 'push! push!'
       result.classList.add('blink')
       result.addEventListener('click', () => {
         location.reload()
